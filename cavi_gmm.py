c087bb77e32d2e08285afed8e2c7443a38b65044
Robin Liu <71776622+roobnloo@users.noreply.github.com>
2022-05-31 18:07:00 -0700

    lint files

--- cavi_gmm.py
+++ cavi_gmm.py
@@ -3,6 +3,7 @@ import math
 from dataclasses import dataclass
 from scipy.stats import norm, uniform
 
+
 @dataclass
 class CaviGmmResult:
     elbo: list
@@ -10,27 +11,37 @@ class CaviGmmResult:
     s2_mat: np.ndarray
     phi_arr: np.ndarray
 
-def elbo(kcat:int, xobs:np.ndarray, sigma, m:np.ndarray, s2:np.ndarray, phi:np.ndarray):
+
+def elbo(kcat: int, xobs: np.ndarray, sigma, m: np.ndarray, s2: np.ndarray,
+         phi: np.ndarray):
     n = len(xobs)
-    t1 = sum(-math.log(2 * math.pi * sigma**2)/2 - mk**2/(2 * sigma**2) for mk in m)
+    t1 = sum(-math.log(2 * math.pi * sigma**2)/2 - mk**2/(2 * sigma**2)
+             for mk in m)
     t2 = n*(-math.log(kcat) - math.log(2 * math.pi)/2)\
-        - sum(phi[i, k] * (s2[k] + (m[k] - xobs[i])**2) for k in range(kcat) for i in range(n)) / 2
+        - sum(phi[i, k] * (s2[k] + (m[k] - xobs[i])**2)
+              for k in range(kcat) for i in range(n)) / 2
     t3 = np.sum(phi * np.log(phi))
     t3 = np.where(np.isnan(t3), 0, t3)
     t4 = sum(-math.log(2 * math.pi * s2k)/2 - 1/2 for s2k in s2)
     return t1 + t2 - t3 - t4
 
-def update_cluster_assgn(kcat:int, m:np.ndarray, s2:np.ndarray, xi):
-    result = np.array([math.exp(m[k] * xi - (s2[k] + m[k]**2) / 2) for k in range(kcat)])
+
+def update_cluster_assgn(kcat: int, m: np.ndarray, s2: np.ndarray, xi):
+    result = np.array([math.exp(m[k] * xi - (s2[k] + m[k]**2) / 2)
+                       for k in range(kcat)])
     return result / sum(result)
 
-def update_center(sigma, phi:np.ndarray, xobs:np.ndarray, k):
+
+def update_center(sigma, phi: np.ndarray, xobs: np.ndarray, k):
     muk = np.dot(phi[:, k], xobs) / (1/(sigma**2) + np.sum(phi[:, k]))
     s2k = 1 / (1/(sigma**2) + np.sum(phi[:, k]))
     return muk, s2k
 
-def initialize(kcat, n, rng:np.random.Generator, max_iter):
-    phi_init = np.reshape(uniform.rvs(size=n * kcat, random_state=rng), (n, kcat))
+
+def initialize(kcat, n, rng: np.random.Generator, max_iter):
+    phi_init = np.reshape(
+        uniform.rvs(size=n * kcat, random_state=rng),
+        (n, kcat))
     phi_sum = phi_init.sum(axis=1)
     phi_arr = np.zeros((n, kcat, max_iter))
     phi_arr[:, :, 0] = phi_init / phi_sum[:, np.newaxis]
@@ -40,7 +51,9 @@ def initialize(kcat, n, rng:np.random.Generator, max_iter):
     var_mat[:, 0] = np.ones(kcat)
     return mean_mat, var_mat, phi_arr
 
-def cavi(xobs:np.ndarray, kcat, sigma, rng:np.random.Generator=None, tol=1e-4, max_iter=1_000):
+
+def cavi(xobs: np.ndarray, kcat, sigma,
+         rng: np.random.Generator = None, tol=1e-4, max_iter=1_000):
     if rng is None:
         rng = np.random.default_rng()
     n = len(xobs)
@@ -50,11 +63,16 @@ def cavi(xobs:np.ndarray, kcat, sigma, rng:np.random.Generator=None, tol=1e-4, m
     iter = 1
     while True:
         for i in range(n):
-            phi_arr[i, :, iter] = update_cluster_assgn(kcat, mean_mat[:, iter-1], s2_mat[:, iter-1], xobs[i])
+            phi_arr[i, :, iter] = update_cluster_assgn(
+                kcat, mean_mat[:, iter-1], s2_mat[:, iter-1], xobs[i])
         for k in range(kcat):
-            mean_mat[k, iter], s2_mat[k, iter] = update_center(sigma, phi_arr[:,:,iter], xobs, k)
-        elbo_list[iter] = elbo(kcat, xobs, sigma, mean_mat[:, iter], s2_mat[:, iter], phi_arr[:, :, iter])
+            mean_mat[k, iter], s2_mat[k, iter] = update_center(
+                sigma, phi_arr[:, :, iter], xobs, k)
+        elbo_list[iter] = elbo(kcat, xobs, sigma, mean_mat[:, iter],
+                               s2_mat[:, iter], phi_arr[:, :, iter])
         if abs(elbo_list[iter] - elbo_list[iter - 1]) < tol:
             break
         iter += 1
-    return CaviGmmResult(elbo_list[1:iter+1], mean_mat[:, 0:iter], s2_mat[:, 0:iter], phi_arr[:, :, 0:iter])
+
+    return CaviGmmResult(elbo_list[1:iter+1], mean_mat[:, 0:iter],
+                         s2_mat[:, 0:iter], phi_arr[:, :, 0:iter])
--- main.py
+++ main.py
@@ -10,8 +10,9 @@ n = 50
 sigma = 6
 
 centers = norm.rvs(scale=sigma, size=kcat, random_state=rng)
-cats = randint.rvs(0, kcat, size = n, random_state=rng)
-xobs = np.array([norm.rvs(loc=centers[c], scale=2, random_state=rng) for c in cats])
+cats = randint.rvs(0, kcat, size=n, random_state=rng)
+xobs = np.array([norm.rvs(loc=centers[c], scale=2, random_state=rng)
+                 for c in cats])
 
 result = cavi(xobs, kcat, sigma, rng=rng)
 elbo_list = result.elbo
@@ -24,9 +25,10 @@ plt.ylabel("ELBO")
 plt.xlabel("Iteration")
 plt.show()
 
-def catmap(m:np.ndarray, centers:np.ndarray):
-    mmask = np.ma.array(m, mask = False)
-    cenmask = np.ma.array(centers, mask = False)
+
+def catmap(m: np.ndarray, centers: np.ndarray):
+    mmask = np.ma.array(m, mask=False)
+    cenmask = np.ma.array(centers, mask=False)
     cm = dict()
     while not all(mmask.mask):
         marg = mmask.argmax()
@@ -36,19 +38,23 @@ def catmap(m:np.ndarray, centers:np.ndarray):
         cenmask.mask[cenarg] = True
     return cm
 
+
 cm = catmap(m, centers)
 cats_assgn = [cm[i] for i in phi.argmax(axis=1)]
 misclassified = sum(cats_assgn != cats)
 print(misclassified)
 print(misclassified / n)
 
+c1 = "#E83845"
+c2 = "#288BA8"
+c3 = "#746AB0"
 x = np.linspace(-20, 15, 100)
 fig, ax = plt.subplots()
-ax.plot(x, norm.pdf(x, m[0], 2), color="#E83845")
-ax.plot(x, norm.pdf(x, m[1], 2), color="#288BA8")
-ax.plot(x, norm.pdf(x, m[2], 2), color="#746AB0")
-colmap = {0:"#E83845", 1:"#746AB0", 2:"#288BA8"}
-ax.scatter(xobs, [0]*n, c=[colmap[cat] for cat in cats], zorder = 100, alpha=0.6)
-ax.vlines(centers, 0, 0.22, colors = ["#E83845", "#746AB0", "#288BA8"], linestyles='dotted')
+ax.plot(x, norm.pdf(x, m[0], 2), color=c1)
+ax.plot(x, norm.pdf(x, m[1], 2), color=c2)
+ax.plot(x, norm.pdf(x, m[2], 2), color=c3)
+colmap = {0: c1, 1: c3, 2: c2}
+ax.scatter(xobs, [0]*n, c=[colmap[cat] for cat in cats], zorder=100, alpha=0.6)
+ax.vlines(centers, 0, 0.22, colors=[c1, c3, c2], linestyles='dotted')
 ax.set_title("Gaussian Mixture Model via Variational Inference")
 plt.show()
